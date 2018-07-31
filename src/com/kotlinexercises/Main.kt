38f64e975db1b15115b1bafe3a0f80a26b35b678
Vinicius N. Coscia <vinicius.coscia@live.com>
2018-07-31 09:34:40 -0300

    Atualização dos arquivos de anotações

--- src/com/kotlinexercises/Main.kt
+++ src/com/kotlinexercises/Main.kt
@@ -1,7 +1,141 @@
 package com.kotlinexercises
 
 fun main(args: Array<String>) {
-    safeCallSample()
+
+}
+
+/**
+ * Varargs é pra mandar uma quantidade variável de parâmetros
+ */
+fun varArgsSample(vararg strings: String){
+    strings.forEach { println(it) }
+
+    var lista = arrayOf("A", "B", "C")
+
+    varArgsSample(*lista) //É POSSÍVEL PASSAR UMA ARRAY USANDO UM ASTERISCO
+}
+
+class Account {
+    var balance = 0.0
+
+    infix fun add(amount: Double){
+        this.balance = amount + amount
+    }
+
+    fun testeAdd() {
+        var conta = Account()
+
+        conta add 100.00
+
+        println(conta.balance)
+    }
+}
+
+fun namedArgumentsSample() {
+    fun printInformacoesPessoa(name: String, age: Int, isMale: Boolean) = println("Nome: $name, idade: $age, Sexo masculino: $isMale")
+
+    printInformacoesPessoa(name = "João", age = 24, isMale = true)
+}
+
+fun localFunctionCalculateArea(width:Int, height: Int) {
+    fun calculateArea(width:Int, height: Int) = width * height
+
+    val area = calculateArea(width, height)
+
+    println(area)
+}
+
+fun singleExpressionFunction(k:Int) = k*k
+
+fun breakingTheLoop() {
+    var i = 0
+    var j = 0
+
+    loopTeste@ for(i in 1..10){
+        for(j in 1..10){
+            if(j==5) break@loopTeste
+        }
+    }
+}
+
+fun forEach(){
+    var list = listOf<Int>(1, 2, 3, 4)
+
+    for(num in list){
+        println(num)
+    }
+
+    list.forEach { println(it) }
+}
+
+fun ranges() {
+    var x = 1
+
+    println(x in 1..10)
+
+    for(i in 1 until 5)
+        println("Until $i")
+
+    for(i in 1..5)
+        println("$i..5")
+
+    for(i in 5 downTo 1)
+        println("$i of 5 down to 1")
+
+    for(i in 1..10 step 2)
+        println("$i 1..10 step 2")
+}
+
+fun whenStatementWithoutArgs() {
+    println(when {
+        0 < 1 -> "Teste 1"
+        0 == 0 -> "teste 2"
+        else -> "Fodasse"
+    })
+}
+
+fun whenStatementWithSmartCasting(x: Any): Boolean {
+    return when(x){
+        is String -> x.startsWith("foo")
+        is Int -> x > 0
+        else -> false
+    }
+}
+
+fun whenStatementWithInOperator(x: Int) {
+    val validNumbers = -30..0
+    println(when (x) {
+        in 1..10 -> "x is in the range"
+        in validNumbers -> "x is valid"
+        else -> "Batata"
+    })
+}
+
+fun whenStatementInVariableReturn(x: Int): Boolean {
+    return when(x){
+        1 -> true
+        2 -> false
+        else -> false
+    }
+}
+
+fun whenStatementInVariable() {
+    val idioma = "Portuguese"
+    val saudacao = when(idioma){
+        "Portuguese" -> "Olá, tudo bem?"
+        "English" -> "Hello, how are you?"
+        "Favelado" -> "E aí mano bráu!!!!!!!!!1111"
+        else -> null
+    }
+
+    println(saudacao)
+}
+
+fun notNumAssentionExclamacaoDupla() {
+    val nullableName: String? = "George"
+    val name:String = nullableName!!
+
+    println(name)
 }
 
 fun safeCallSample(){
@@ -98,7 +232,13 @@ fun smartCastExample(var1: Any){
  * Exemplo de Explicit Casting
  */
 fun explictCastSample(var1: Any) {
-    var var2: String = var1 as String
+    var var2: String? = var1 as? String
+
+    when(var2){
+        null -> var2 = "Variável estava nula por erro no cast"
+    }
+
+    println(var2)
 }
 
 class Employee(val name: String, val manager: Employee? = null)
